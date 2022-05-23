3d280b76607d5cfc707b2ad83a9697aedee71b94
Stmarco <S.tmarco@alumnos.upm.es>
2022-05-23 12:50:59 +0200

    Propiedades y creación BBDD

---
+++ src/main/java/com/example/entities/Departamento.java
@@ -0,0 +1,33 @@
+package com.example.entities;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Data
+@Builder
+public class Departamento implements Serializable{
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private long id;
+   private String nombre;
+
+   //mappedBy pones de donde es la relación padre
+   @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST, mappedBy = "departamento")
+   private List<Empleado> empleados;
+
+    
+
+}
