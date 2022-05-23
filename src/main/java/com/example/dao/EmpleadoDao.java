dd1b1365e48bfed281f022d6c1558131fa7539ee
Stmarco <S.tmarco@alumnos.upm.es>
2022-05-23 14:57:54 +0200

    Controlador y vista

--- src/main/java/com/example/dao/EmpleadoDao.java
+++ src/main/java/com/example/dao/EmpleadoDao.java
@@ -11,6 +11,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EmpleadoDao extends JpaRepository<Empleado, Long> {
     //Query para buscar empleados por un salario que damos por parametro
-    @Query(value = "select salario from Empleado fetch :idEmpleado", nativeQuery = true)
+    @Query(value = "select * from Empleado fetch :salario", nativeQuery = true)
     public List<Empleado> findBySalario(double salario);
 }
