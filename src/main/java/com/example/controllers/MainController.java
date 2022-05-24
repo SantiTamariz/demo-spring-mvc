318e8538c80d241fa386c0b6ca1f17e134c7394a
Stmarco <S.tmarco@alumnos.upm.es>
2022-05-24 09:20:52 +0200

    Modelo creado listarEmpleados

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -1,16 +1,45 @@
 package com.example.controllers;
 
+import com.example.services.EmpleadoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/")
 public class MainController {
 
+    //Se hace inyección de dependecia con SpringBOoot en @Autowired 
+    //Crea el elemento bean empleadoService para poder usar sus métodos sin necesidad de instanciarlo
+    //Así solo lo instancia cuando es necesario y lo usa SpringBoot
+    @Autowired
+    private EmpleadoService empleadoService;
+
+    //El metodo recibe un modelo del framework vacio 
     @GetMapping("/listar")
-    public String listarEmpleado(){
+    public String listarEmpleadoMalo(Model model){
+
+        //Método añade los datos al modelo pidiendolos al servicio por el atributo empleados
+        model.addAttribute("empleados", empleadoService.findAll());
+
+        //Devolvemos la lista.html que va a recoger el modelo.
         return "listar";
     }
     
+    //Crear un modelo 
+    public ModelAndView listarEmpleado(){
+
+        //Modelo nuevo asignado a la vista
+        ModelAndView maw = new ModelAndView("listar");
+
+        //Añadir al modelo del atributo empleados cogidos los datos del servicio
+        maw.addObject("empledos", empleadoService.findAll());
+
+        //Retornar modelo a la vista
+        return maw;
+    }
 }
